1671e9c3e82d2e6839f75bca6e9f9ba619db446d
Wendy <50072566+vendaciki@users.noreply.github.com>
2023-02-05 18:53:37 +0100

    Add files via upload

---
+++ js/virtualList.js
@@ -0,0 +1,10 @@
+var virtualList = [
+{nazev: "	Mobilis in mobili	" , GC: "	GC7B8CJ	" , coords: [	49.1976333, 16.6075333	]},
+
+
+
+
+
+
+
+];
---
+++ js/webcamList.js
@@ -0,0 +1,10 @@
+var webcamList = [
+
+
+
+
+
+
+
+
+];
